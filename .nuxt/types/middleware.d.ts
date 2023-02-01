d362166f1360947d5759033b36d6936f2c297761
crazzyboy420 <raselbabu34.bd@gmail.com>
2023-02-01 22:58:18 +0600

    quantity update

--- .nuxt/types/middleware.d.ts
+++ .nuxt/types/middleware.d.ts
@@ -1,6 +1,6 @@
 import type { NavigationGuard } from 'vue-router'
 export type MiddlewareKey = string
-declare module "/Users/asif/Downloads/Web Design & Development/Client project/safwat-nuxt-frontend/node_modules/nuxt/dist/pages/runtime/composables" {
+declare module "D:/my work/Documents/safwat-nuxt-frontend/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
   }
